47e5c1f82c3d0642fa3a1e43af9dc72eb3afba93
Serge Najimov <serge.najimov@peruzziservices.com>
2022-08-16 11:22:15 +0200

    added configs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(5000);
+  const config = await app.get(ConfigService);
+  const port = config.get<number>('API_PORT');
+  await app.listen(port || 5000, () => {
+    Logger.log(`App started on port: ${port}`);
+  });
 }
 bootstrap();
